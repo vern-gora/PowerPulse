1a69fd83182390a3c712724de6d38bd7cf45d197
Roman0064 <122211211+Roman0064@users.noreply.github.com>
2023-11-12 16:51:05 +0200

    Update StatisticsInfo.jsx

--- src/components/StatisticsInfo/StatisticsInfo.jsx
+++ src/components/StatisticsInfo/StatisticsInfo.jsx
@@ -1,8 +1,31 @@
 import css from './statisticsInfo.module.css';
 import sprite from '../../images/svg/sprite.svg';
+import axios from 'axios';
+import { useEffect } from 'react';
+
+axios.defaults.baseURL = "https://powerpulse-backend.onrender.com";
 
 const StatisticInfo = ({videos, calories}) => {
+    useEffect(()=> {
+        getVideo();
+        getCalories()
+    }, []);
 
+    async function getVideo() {
+        try {
+          const vid = await axios.get('/statistic/video');
+          console.log(vid.data.videos);
+        } catch (error) {
+          console.error(error);
+        }
+    };
+    async function getCalories() {
+        try {
+          const calories = await axios.get('/statistic/calories');
+        } catch (error) {
+          console.error(error);
+        }
+    };
     return (
         <div className={css.wrapper}>  
             <div className={css.videos_wrapper}>
