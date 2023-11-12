92c773835f98e6d559d23eda799aa8a3fc44889f
vern-gora <danvern99@gmail.com>
2023-11-12 23:33:26 +0200

    new store

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from 'redux/store';
-// import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter basename='/PowerPulse'>
-        {/* <PersistGate loading={null} persistor={persistor}> */}
+        <PersistGate loading={null} persistor={persistor}>
           <App />
-        {/* </PersistGate> */}
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
