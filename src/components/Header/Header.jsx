16b47a8889b6d65aba8defb317c042b65bfece82
giligran <kuprykovrodion@gmail.com>
2023-11-13 21:11:00 +0200

    some header css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import css from './Header.module.css';
+import svg from '../../images/svg/sprite.svg';
+
 function Header() {
   return (
     <>
-      <p className={css.header}>headerstyle tetst</p>
-      <NavLink to="/"></NavLink>
+      <NavLink to="/" className={css.header_logo}>
+        <svg className={css.icon_logo}>
+          <use href={svg + `#logo_icon`}></use>
+        </svg>
+        <svg className={css.icon_text}>
+          <use href={svg + `#logo_text`}></use>
+        </svg>
+      </NavLink>
     </>
   );
 }
