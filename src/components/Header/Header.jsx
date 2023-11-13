55d151431efb7b48dce996e1f3cfb8ebec5ed307
Rodion Kuprykov <118477139+giligran@users.noreply.github.com>
2023-11-13 18:03:30 +0000

    Update 4 files

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
-
+import { NavLink } from 'react-router-dom';
+import css from './Header.module.css';
 function Header() {
-  return <div></div>;
+  return (
+    <>
+      <p className={css.header}>headerstyle tetst</p>
+      <NavLink to="/"></NavLink>
+    </>
+  );
 }
 export default Header;
