bbad9f9debaec90a0392178bb5485a3b0013630c
vdbeast <122833284+vdbeast@users.noreply.github.com>
2023-11-13 22:39:16 +0200

    completed signin logic

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,22 +1,43 @@
 import { NavLink } from "react-router-dom";
 import style from './SignInForm.module.css'
+import { useDispatch } from "react-redux";
+import { logIn } from "redux/auth/operations";
 
 const SignInForm = () => {
+    const dispatch = useDispatch();
+
+    const handleSubmit = (event) => {
+
+        event.preventDefault();
+
+        const email = event.currentTarget.elements.userEmail.value;
+        const password = event.currentTarget.elements.userPassword.value;
+
+        const formData = {
+            email,
+            password,
+        };
+        
+        console.log(formData)
+
+        dispatch(logIn(formData));
+    }
+
     return (
         <div className={style.loginContainer}>
             <div className={style.formContainer}>
                 <h1 className={style.title}>Sign In</h1>
                 <p className={style.titleDescription}>Welcome! Please enter your credentials to login to the platform:</p>
-                <form className={style.form}>
+                <form onSubmit={handleSubmit} className={style.form}>
                     <label>
                         <input type="email" required placeholder="Email" name="userEmail" className={style.input} />
                     </label>
                     <label>
                         <input type="password" required placeholder="Password" name="userPassword" minLength={7} className={style.input} />
                     </label>
+                    <button className={style.btn}>Sign In</button>
                 </form>
             </div>
-            <button className={style.btn}>Sign In</button>
             <p className={style.description}>Don’t have an account?&nbsp;
                 <NavLink to={'/signup'} className={style.link}>Sign Up</NavLink>
             </p>
