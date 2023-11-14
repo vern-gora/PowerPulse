879a5f149c4ecc3126edc18624cc976b422c6217
vern-gora <danvern99@gmail.com>
2023-11-14 15:46:10 +0200

    add profile

---
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserForm from 'components/UserForm/UserForm';
+
+const ProfilePage = () => {
+  return (
+    <>
+      <UserForm/>
+    </>
+  );
+};
+
+export default ProfilePage;
