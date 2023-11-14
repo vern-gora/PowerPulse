96059b2ff1b306187842a5db973ca87b4c7c5ab6
Roman0064 <122211211+Roman0064@users.noreply.github.com>
2023-11-15 01:09:03 +0200

    min Validaton

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -2,39 +2,50 @@ import { NavLink } from "react-router-dom";
 import style from './SignInForm.module.css'
 import { useDispatch } from "react-redux";
 import { logIn } from "redux/auth/operations";
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
 const SignInForm = () => {
     const dispatch = useDispatch();
 
-    const handleSubmit = (event) => {
-
-        event.preventDefault();
-
-        const email = event.currentTarget.elements.userEmail.value;
-        const password = event.currentTarget.elements.userPassword.value;
-
-        const formData = {
-            email,
-            password,
-        };
-
-        dispatch(logIn(formData));
-    }
+    const SignInSchema = Yup.object().shape({
+        email: Yup.string()
+          .min(2, 'Too Short Email!')
+          .max(50, 'Too Long Email!')
+          .email('Invalid email !!!')
+          .required('Required'),
+        password: Yup.string().required('Required'),
+      });
 
     return (
         <div className={style.loginContainer}>
             <div className={style.formContainer}>
                 <h1 className={style.title}>Sign In</h1>
                 <p className={style.titleDescription}>Welcome! Please enter your credentials to login to the platform:</p>
-                <form onSubmit={handleSubmit} className={style.form}>
-                    <label>
-                        <input type="email" required placeholder="Email" name="userEmail" className={style.input} />
-                    </label>
-                    <label>
-                        <input type="password" required placeholder="Password" name="userPassword" minLength={7} className={style.input} />
-                    </label>
-                    <button className={style.btn}>Sign In</button>
-                </form>
+                <Formik 
+                    initialValues={{
+                        email: '',
+                        password: '',
+                      }}
+                      validationSchema={SignInSchema}
+                      onSubmit={values => {
+                        dispatch(logIn(values));
+                      }}
+                >
+                    {({ errors, touched }) => (
+                        <Form className={style.form}>
+                            <label>
+                                <Field type="email" required placeholder="Email" name="email" className={style.input} />
+                                {errors.email && touched.email ? (<div className={style.message}>{errors.email}</div>) : null}
+                            </label>
+                            <label>
+                                <Field type="password" required placeholder="Password" name="password" minLength={7} className={style.input} />
+                                {errors.password && touched.password ? (<div className={style.message}>{errors.password}</div>) : null}
+                            </label>
+                            <button type="submit" className={style.btn}>Sign In</button>
+                        </Form>
+                    )}
+                </Formik>
             </div>
             <p className={style.description}>Don’t have an account?&nbsp;
                 <NavLink to={'/signup'} className={style.link}>Sign Up</NavLink>
