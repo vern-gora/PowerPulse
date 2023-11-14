ea2cd43551ec9957e544ebd3dd645c83e4ba3f73
vern-gora <danvern99@gmail.com>
2023-11-14 21:09:03 +0200

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -20,7 +20,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ['token'],
+  whitelist: ['auth'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
