f02ca0e5fa66536c3ff78a9365acc29f9aa9c785
giligran <kuprykovrodion@gmail.com>
2023-11-18 19:09:25 +0200

    Update operations.js

---
+++ src/redux/diary/operations.js
@@ -0,0 +1,55 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+const setAuthHeader = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+export const fetchFoodAndExercises = createAsyncThunk(
+  'food/fetchFood',
+  async (date, thunkAPI) => {
+    try {
+      const state = thunkAPI.getState();
+      const storedToken = state.auth.token;
+
+      setAuthHeader(storedToken);
+      const response = await axios.get(`/diary?date=${date}`);
+      return response.data;
+    } catch (e) {
+      toast.error('Some error occured. Please try again later');
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const deleteFood = createAsyncThunk(
+  'food/deleteFood',
+  async (_id, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/diary/food/${_id}`);
+
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const addProductToDiary = createAsyncThunk(
+  'diary/addProductToDiary',
+  async (data, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const storedToken = state.auth.token;
+
+    if (storedToken === null) {
+      return thunkAPI.rejectWithValue();
+    }
+    try {
+      setAuthHeader(storedToken);
+      const res = await axios.post('/diary/food', data);
+      return res.data.result;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
