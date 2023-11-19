f6075d4be120e3640f0b0152e64a9b74b4bd8731
Prosukov Oleksandr <122283728+ProsukovOleksandr@users.noreply.github.com>
2023-11-19 21:48:28 +0300

    fixes

--- src/pages/DiaryPage/DiaryPage.jsx
+++ src/pages/DiaryPage/DiaryPage.jsx
@@ -1,7 +1,8 @@
 import { DayProducts } from 'components/DayProdcuts/DayProdcuts';
 import DayExercises from 'components/DayExercises/DayExercises';
 import DayDashboard from 'components/DayDashboard/DayDashboard';
-import { DaySwitch } from 'components/DaySwitch/DaySwitch';
+import svg from '../../images/svg/sprite.svg';
+import StyledDatepicker from 'components/Calendar/StyledDatepicker';
 import css from './DiaryPage.module.css';
 import { useDispatch /*useSelector*/ } from 'react-redux';
 import { useState, useEffect } from 'react';
@@ -13,6 +14,7 @@ import {
   selectDailyPhysicalActivity,
 } from 'redux/diary/selectors';
 import { fetchFoodAndExercises } from 'redux/diary/operations';
+import { refreshUser } from 'redux/auth/operations';
 /*import {
   selectConsumedFood,
   selectCurrentData,
@@ -26,7 +28,7 @@ import { fetchFoodAndExercises } from 'redux/diary/operations';
 }*/
 const DiaryPage = () => {
   const [prData, setPrData] = useState([]);
-  const [exData, setExData] = useState(null);
+  const [exData, setExData] = useState([]);
 
   const dispatch = useDispatch();
 
@@ -43,51 +45,19 @@ const DiaryPage = () => {
   const dailyRateCalories = useSelector(selectDailyCalorieIntake);
   const dailySportMin = useSelector(selectDailyPhysicalActivity);
   useEffect(() => {
-    /*const date = getCurrentDate();*/
-    dispatch(fetchFoodAndExercises('19/11/2023'));
+    const date = getCurrentDate();
+    dispatch(fetchFoodAndExercises(date));
     setPrData([...productsData]);
     setExData([...exersizesData]);
-  }, [dispatch, productsData, exersizesData]);
-console.log(exData);
-console.log(exersizesData);
-  /*const exampleExData = [
-    {
-      _id: '6559e876f22ef270700e05e4',
-      exercise: {
-        bodyPart: 'SUSSYYY',
-        equipment: 'barbell',
-        name: 'barbell rack pull',
-        target: 'glutes',
-      },
 
-      date: '19/11/2023',
-      time: '30',
-      calories: 150,
-      owner: '654cd9eb473e0780072ec53f',
-      v: 0,
-    },
-    {
-      _id: '6559e88bf22ef270700e05e8',
-      exercise: {
-        bodyPart: 'upper arms',
-        equipment: 'dumbbell',
-        name: 'dumbbell incline hammer curl',
-        target: 'biceps',
-      },
-      date: '19/11/2023',
-      time: '30',
-      calories: 150,
-      owner: '654cd9eb473e0780072ec53f',
-      v: 0,
-    },
-  ];*/
+  }, [dispatch]);
   const bodyData = {
     dailyRateCalories,
     dailySportMin,
   }
   const diaryForDasboard = {};
-  if (prData) {
-    const consumedCalories = prData.reduce((accumulator, currentValue) => {
+  if ([...productsData]) {
+    const consumedCalories = [...productsData].reduce((accumulator, currentValue) => {
       return accumulator + currentValue['calories'];
     }, 0);
     diaryForDasboard.consumedCalories = consumedCalories;
@@ -105,41 +75,30 @@ console.log(exersizesData);
     }, 0);
     diaryForDasboard.timeSport = timeSport;
   }
-
-
-  /*
-  [
-    {
-      _id:"",
-      exercise:{
-        bodyPart:"",
-        equipment:"",
-        name:"",
-        target:"",
-      },
-      burnedCalories:"",
-      time:"",
-    },
-    {
-      ...
-    }
-  ]
-  */
   return (
     <section className={css.diaryPage}>
       <div className={css.topBar}>
         <h1 className={css.header}>Diary</h1>
-        <DaySwitch />
+        <StyledDatepicker />
       </div>
+      <div className={css.bottomBar}>
+      <div className={css.bottomRightBar}>
       <DayDashboard diary={diaryForDasboard} bodyData={bodyData}/>
       <div className={css.reminder}>
+        <svg className={css.reminderIcon} width='50px' height='24px'>
+          <use href={svg  + "#exclamation_mark_icon"}></use>
+        </svg>
       <p className={css.reminderText}>
           Record all your meals in the calorie diary every day. This will help
           you be aware of your nutrition and make informed choices.
         </p>
       </div>
+      </div>
+      <div className={css.diaryLists}>
       <DayProducts productsData={[...productsData]} />
       <DayExercises doneExercises={[...exersizesData]} date={getCurrentDate()} />
+      </div>
+      </div>
 
     </section>
   );
